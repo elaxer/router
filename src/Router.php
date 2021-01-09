3477427469ec9e118ebcca0ab2e80914cd598aa7
elaxer <elaxerdev@gmail.com>
2021-01-09 18:06:17 +0600

    Removed Psr7 dependency, findRoute returns only route

--- src/Router.php
+++ src/Router.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Elaxer\Router;
 
-use Psr\Http\Message\RequestInterface;
 use Elaxer\Router\Parser\{InvalidCharacterException, PatternParser};
 
 /**
@@ -15,53 +14,44 @@ use Elaxer\Router\Parser\{InvalidCharacterException, PatternParser};
 class Router
 {
     /**
-     * @var array array of routes
+     * @var array<Route> array of routes
      */
     protected array $routes = [];
 
     /**
      * Adds a route to the list
      *
-     * @param string $method route method
-     * @param string $pattern regexp route pattern
-     * @param mixed $handler handler for this route
-     * @return Route created route
+     * @param Route
+     * @return void
      */
-    public function addRoute(string $method, string $pattern, $handler): Route
+    public function addRoute(Route $route): void
     {
-        $route = new Route($method, $pattern, $handler);
         $this->routes[] = $route;
-
-        return $route;
     }
 
     /**
      * Finds the required route by the HTTP request and returns it along with the variables
      *
-     * @param RequestInterface $request PSR-7 HTTP request
-     * @return array associative array, where "route" is the route found,
-     * and "Wars" are variables extracted from the request URI
+     * @param string $urlPath the path to which routes will be matched
+     * @param string $method request method to which routes methods will be matched
+     * @return Route|null returns route if found, returns null if route not found
      * @throws InvalidCharacterException
-     * @throws RouteNotFoundException
      */
-    public function findRoute(RequestInterface $request): array
+    public function findRoute(string $urlPath, string $method): ?Route
     {
         foreach ($this->routes as $route) {
-            if ($route->method !== $request->getMethod()) {
+            if ($route->getMethod() !== $method) {
                 continue;
             }
 
-            $parameters = PatternParser::getParameters($route->pattern);
-            $regexp = PatternParser::makeRegexpFromPattern($route->pattern, $parameters);
+            $parameters = PatternParser::getParameters($route->getPattern());
+            $regexp = PatternParser::makeRegexpFromPattern($route->getPattern(), $parameters);
 
-            $matches = [];
-            if (preg_match($regexp, $request->getUri()->getPath(), $matches)) {
-                $vars = array_filter($matches, fn ($name) => !is_int($name), ARRAY_FILTER_USE_KEY);
-
-                return ['route' => $route, 'vars' => $vars];
+            if (preg_match($regexp, $urlPath)) {
+                return $route;
             }
         }
 
-        throw new RouteNotFoundException('Route not found');
+        return null;
     }
 }
